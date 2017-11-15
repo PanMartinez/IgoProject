5057cf0360d7eafaef1bfca120fc22b1cd1306a6
Marcin Płotka <marcinplotka88@gmail.com>
2017-11-15 11:42:33 +0100

    models and migrations

--- IgoProject/app1/models.py
+++ IgoProject/app1/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=64, verbose_name="name")
+    country = models.CharField(max_length=64, verbose_name="country")
+    founded = models.IntegerField(null=True, verbose_name="founded")
+    logo = models.ImageField(upload_to="static/images", blank=True, null=True)
+    description = models.TextField()
+    workers = models.ManyToManyField(User, through= "Members")
+
+
+class Members(models.Model):
+    company = models.ForeignKey(Company)
+    worker = models.ForeignKey(User)
+
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 IgoProject
 
-This is Django project in which I'm going do develop my Django skills.
-I'm not sure where this will take me, but for now. I go for it.
+This is CRM project in which I'm going do develop my Django skills.
+I'm not sure where this will take me, but for now...
+I go for it :)
